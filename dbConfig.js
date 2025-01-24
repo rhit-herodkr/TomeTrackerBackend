a721624bfec483479abeea25449dd42605614695
herodkr <herodkr@rose-hulman.edu>
2025-01-24 11:13:12 -0500

    Made the connection string secure

--- dbConfig.js
+++ dbConfig.js
@@ -1,2 +1,8 @@
-const connectionString="server=golem.csse.rose-hulman.edu;Database=myDB;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
- module.exports=connectionString;
+// Load environment variables
+require('dotenv').config();
+
+// Build the connection string
+const connectionString = `server=${process.env.DB_SERVER};Database=${process.env.DB_NAME};User Id=${process.env.DB_USER};Password=${process.env.DB_PASSWORD};Driver={SQL Server Native Client 11.0}`;
+
+// Export the connection string
+module.exports = connectionString;
